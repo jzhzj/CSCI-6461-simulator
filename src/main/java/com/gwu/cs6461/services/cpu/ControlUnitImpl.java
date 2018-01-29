9cb3b5d97f5668b8e0fc0a1dcd7204b964472c9f
Warren <1988warren@gmail.com>
2018-01-29 15:23:21 -0500

    Implement control unit.

--- src/main/java/com/gwu/cs6461/services/cpu/ControlUnitImpl.java
+++ src/main/java/com/gwu/cs6461/services/cpu/ControlUnitImpl.java
@@ -1,4 +1,21 @@
 package com.gwu.cs6461.services.cpu;
 
+import com.gwu.cs6461.services.instruction.Instruction;
+
 public class ControlUnitImpl implements ControlUnit {
+
+    @Override
+    public Instruction fetch() {
+        return null;
+    }
+
+    @Override
+    public void decode(Instruction instruction) {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
 }
