8fade3f72c2f98f468086e72cde343fbd30ac932
Warren <1988warren@gmail.com>
2018-01-29 15:30:23 -0500

    To be implemented.

--- src/main/java/com/gwu/cs6461/services/cpu/alu/ALUImpl.java
+++ src/main/java/com/gwu/cs6461/services/cpu/alu/ALUImpl.java
@@ -1,5 +1,62 @@
 package com.gwu.cs6461.services.cpu.alu;
 
+/**
+ * TODO to be implemented.
+ */
 public class ALUImpl implements ALU {
 
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public void addWithCarry() {
+
+    }
+
+    @Override
+    public void subtract() {
+
+    }
+
+    @Override
+    public void subtractWithBorrow() {
+
+    }
+
+    @Override
+    public void negate() {
+
+    }
+
+    @Override
+    public void increment() {
+
+    }
+
+    @Override
+    public void decrement() {
+
+    }
+
+    @Override
+    public void passThrough() {
+
+    }
+
+    @Override
+    public void and() {
+
+    }
+
+    @Override
+    public void or() {
+
+    }
+
+    @Override
+    public void not() {
+
+    }
 }
--- src/main/java/com/gwu/cs6461/services/cpu/registers/IDXRImpl.java
+++ src/main/java/com/gwu/cs6461/services/cpu/registers/IDXRImpl.java
@@ -3,6 +3,16 @@ package com.gwu.cs6461.services.cpu.registers;
 /**
  * Index Register
  * Index Registers are used for data movement.
+ * TODO to be implemented
  */
-public class IDXRImpl implements Register<> {
+public class IDXRImpl implements Register<Object> {
+    @Override
+    public void write(Object data) {
+
+    }
+
+    @Override
+    public Object read() {
+        return null;
+    }
 }
