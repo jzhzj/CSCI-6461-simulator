22c3939256b02957308c8b11db01cf0dfcf396c6
Warren <1988warren@gmail.com>
2018-01-29 16:29:39 -0500

    Define methods for Binary.

--- src/main/java/com/gwu/cs6461/util/Binary.java
+++ src/main/java/com/gwu/cs6461/util/Binary.java
@@ -1,8 +1,14 @@
 package com.gwu.cs6461.util;
 
 /**
- * TODO not yet designed.
+ * Defines what a Binary ADT does
  */
 public interface Binary {
+    int getDecimalValue();
+    String getBinary();
+    String getHex();
+
+    void setValue(int literalValue);
+//    void setValue(String literalValue);
 
 }
