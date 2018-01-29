0e05fcd685a2ec4643e3d1417b41a9e736755107
Warren <1988warren@gmail.com>
2018-01-29 15:12:12 -0500

    Implement PC reg.

--- src/main/java/com/gwu/cs6461/services/cpu/registers/IARImpl.java
+++ src/main/java/com/gwu/cs6461/services/cpu/registers/IARImpl.java
@@ -1,11 +1,24 @@
 package com.gwu.cs6461.services.cpu.registers;
 
+import com.gwu.cs6461.services.dram.DRAMAddress;
+
 /**
  * Instruction Address Register
  * a.k.a Program Counter (PC), Instruction Pointer (IP)
  * It holds the address of the next instruction to be fetched and executed.
  * When instruction is fetched, the value of PC is automatically incremented and it points to the address of next instruction
  */
-public class IARImpl implements Register<> {
+public class IARImpl implements Register<DRAMAddress> {
 
+    private DRAMAddress nextInsAddress;
+
+    @Override
+    public void write(DRAMAddress address) {
+        nextInsAddress = address;
+    }
+
+    @Override
+    public DRAMAddress read() {
+        return nextInsAddress;
+    }
 }
