e0ae06d28151c79e43e0ab7cece862e20ea7697b
Warren <1988warren@gmail.com>
2018-01-28 21:53:38 -0500

    Add generic method read() and write().

--- src/main/java/com/gwu/cs6461/services/cpu/registers/Register.java
+++ src/main/java/com/gwu/cs6461/services/cpu/registers/Register.java
@@ -3,5 +3,17 @@ package com.gwu.cs6461.services.cpu.registers;
 /**
  * Defines what register does
  */
-public interface Register {
+public interface Register<T> {
+
+    /**
+     * Write data to register
+     * @param data data to be written to register
+     */
+    void write(T data);
+
+    /**
+     * Read data from register
+     * @return data in register
+     */
+    T read();
 }
