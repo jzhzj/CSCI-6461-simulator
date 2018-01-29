c41e97a389f29a64a550da81b04904c8ff938d54
Warren <1988warren@gmail.com>
2018-01-29 15:26:46 -0500

    Implement GPR reg.

--- src/main/java/com/gwu/cs6461/services/cpu/registers/GPRImpl.java
+++ src/main/java/com/gwu/cs6461/services/cpu/registers/GPRImpl.java
@@ -1,11 +1,24 @@
 package com.gwu.cs6461.services.cpu.registers;
 
+import com.gwu.cs6461.services.dram.DRAMData;
+
 /**
  * General Purpose Register
  * Accumulator register is used to perform arithmetic and logical operations.
  * It holds the initial data to be operated upon, and the final result for processing.
  * The results of arithmetic operations are returned to accumulator for transfer to the main memory through buffer register.
  */
-public class GPRImpl implements Register<>{
+public class GPRImpl implements Register<DRAMData>{
 
+    private DRAMData operationResult;
+
+    @Override
+    public void write(DRAMData data) {
+        operationResult = data;
+    }
+
+    @Override
+    public DRAMData read() {
+        return operationResult;
+    }
 }
