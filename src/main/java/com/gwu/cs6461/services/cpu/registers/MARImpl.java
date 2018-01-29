86028e83cc3597707f32dd70b010f97aad91d92c
Warren <1988warren@gmail.com>
2018-01-29 15:16:49 -0500

    Implement MAR reg.

--- src/main/java/com/gwu/cs6461/services/cpu/registers/MARImpl.java
+++ src/main/java/com/gwu/cs6461/services/cpu/registers/MARImpl.java
@@ -1,10 +1,23 @@
 package com.gwu.cs6461.services.cpu.registers;
 
+import com.gwu.cs6461.services.dram.DRAMAddress;
+
 /**
  * Memory Address Register
  * It holds the address of active memory location.
  * When CPU wants to store or read data from memory, CPU stores the required address of memory location in MAR.
  */
-public class MARImpl implements Register<>{
+public class MARImpl implements Register<DRAMAddress>{
 
+    private DRAMAddress activeAddress;
+
+    @Override
+    public void write(DRAMAddress address) {
+        activeAddress = address;
+    }
+
+    @Override
+    public DRAMAddress read() {
+        return activeAddress;
+    }
 }
