d8669ed6198a59f4a4222f78ea390eaefa968d33
Warren <1988warren@gmail.com>
2018-01-29 15:14:25 -0500

    Implement IR reg.

--- src/main/java/com/gwu/cs6461/services/cpu/registers/IRImpl.java
+++ src/main/java/com/gwu/cs6461/services/cpu/registers/IRImpl.java
@@ -1,9 +1,22 @@
 package com.gwu.cs6461.services.cpu.registers;
 
+import com.gwu.cs6461.services.instruction.Instruction;
+
 /**
  * Instruction Register
  * It holds the current instruction that is being executed
  */
-public class IRImpl implements Register<>{
+public class IRImpl implements Register<Instruction>{
 
+    private Instruction currentIns;
+
+    @Override
+    public void write(Instruction instruction) {
+        currentIns = instruction;
+    }
+
+    @Override
+    public Instruction read() {
+        return currentIns;
+    }
 }
