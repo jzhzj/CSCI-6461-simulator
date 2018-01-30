729a81226b3a96224579f3df7bc5597a0156aedd
Warren <1988warren@gmail.com>
2018-01-30 16:19:03 -0500

    Add test cases.

--- src/test/java/com/gwu/cs6461/services/dram/TestDRAMAddress.java
+++ src/test/java/com/gwu/cs6461/services/dram/TestDRAMAddress.java
@@ -1,5 +1,31 @@
 package com.gwu.cs6461.services.dram;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 public class TestDRAMAddress {
 
+    @Rule
+    public ExpectedException thrown= ExpectedException.none();
+
+    private DRAMAddress dramAddress = new DRAMAddress();
+
+    @Test
+    public void testSetValueInRange(){
+        dramAddress.setValue(DRAMAddress.MIN_VALUE);
+        dramAddress.setValue(DRAMAddress.MAX_VALUE);
+    }
+
+    @Test
+    public void testSetValueAboveRange(){
+        thrown.expect(IllegalArgumentException.class);
+        dramAddress.setValue(DRAMAddress.MAX_VALUE + 1);
+    }
+
+    @Test
+    public void testSetValueBelowRange() {
+        thrown.expect(IllegalArgumentException.class);
+        dramAddress.setValue(DRAMAddress.MIN_VALUE - 1);
+    }
 }
