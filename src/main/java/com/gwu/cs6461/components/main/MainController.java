19bdf006337d7ae60a9dd3ea7c53597658ca619a
Warren <1988warren@gmail.com>
2018-02-02 15:08:10 -0500

    Component id and event binding.

--- src/main/java/com/gwu/cs6461/components/main/MainController.java
+++ src/main/java/com/gwu/cs6461/components/main/MainController.java
@@ -3,16 +3,18 @@ package com.gwu.cs6461.components.main;
 import com.gwu.cs6461.services.*;
 import com.gwu.cs6461.services.cpu.registers.MARImpl;
 import com.gwu.cs6461.services.cpu.registers.Register;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
 import java.util.Observable;
 import java.util.Observer;
 
-public class MainController implements Observer{
+public class MainController implements Observer {
 
     private MachineImpl machine = MachineImpl.getInstance();
 
@@ -22,35 +24,110 @@ public class MainController implements Observer{
     }
 
     @FXML
-    private Button powerButton;
+    private TextField pcTextField;
+    @FXML
+    private TextField marTextField;
+    @FXML
+    private TextField mbrTextField;
+    @FXML
+    private TextField ccTextField;
+    @FXML
+    private TextField mfrTextField;
+    @FXML
+    private TextField r0TextField;
+    @FXML
+    private TextField r1TextField;
+    @FXML
+    private TextField r2TextField;
+    @FXML
+    private TextField r3TextField;
+    @FXML
+    private TextField x1TextField;
+    @FXML
+    private TextField x2TextField;
+    @FXML
+    private TextField x3TextField;
+    @FXML
+    private TextField ramAddressTextField;
+    @FXML
+    private TextField ramValueTextField;
+
 
     @FXML
-    private ListView<String> memoryListView;
+    void handleDebugButtonClick(MouseEvent mouseEvent) {
+        Button btn = (Button) mouseEvent.getSource();
+        switch (btn.getId()) {
+            case "ILPButton":
+                MARImpl.getInstance().reset();
+                machine.ipl();
+                break;
+            case "RUNButton":
+                machine.run();
+                break;
+            case "HALTButton":
+                machine.halt();
+                break;
+            case "SSButton":
+                machine.singleStep();
+                break;
+            default:
+        }
 
-    @FXML
-    void handleIPLButtonClick(MouseEvent mouseEvent) {
-        MARImpl.getInstance().reset();
-        machine.ipl();
     }
 
     @FXML
-    void handleSingleStepButtonClick(MouseEvent mouseEvent) {
-        machine.singleStep();
+    void handleDRAMButtonClick(MouseEvent mouseEvent) {
+        Button btn = (Button) mouseEvent.getSource();
+        switch (btn.getId()) {
+            case "ReadMemButton":
+                break;
+            case "WriteMemButton":
+                break;
+            default:
+        }
     }
 
     @FXML
-    void handleRunButtonClick(MouseEvent mouseEvent) {
-        machine.run();
-    }
+    void handleRegisterButtonClick(MouseEvent mouseEvent) {
+        Button btn = (Button) mouseEvent.getSource();
+        switch (btn.getId()) {
+            case "PCButton":
 
-    @FXML
-    void handleHaltButtonClick(MouseEvent mouseEvent) {
-        machine.halt();
+                break;
+            case "MARButton":
+
+                break;
+            case "MBRButton":
+
+                break;
+            case "R0Button":
+
+                break;
+            case "R1Button":
+
+                break;
+            case "R2Button":
+
+                break;
+            case "R3Button":
+
+                break;
+            case "X1Button":
+
+                break;
+            case "X2Button":
+
+                break;
+            case "X3Button":
+
+                break;
+            default:
+        }
     }
 
     @Override
     public void update(Observable o, Object arg) {
-        if(o instanceof MARImpl){
+        if (o instanceof MARImpl) {
             System.out.println("o = [" + o + "], arg = [" + arg + "]");
         } else {
             System.out.println("not");
