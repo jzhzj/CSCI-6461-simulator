c6c92b20f401dfe8e1a3ebd078103f4a24316755
Warren <1988warren@gmail.com>
2018-02-03 23:53:28 -0500

    Update UI.

--- src/main/java/com/gwu/cs6461/App.java
+++ src/main/java/com/gwu/cs6461/App.java
@@ -1,7 +1,6 @@
 package com.gwu.cs6461;
 
 import com.gwu.cs6461.services.cpu.CPUImpl;
-import com.gwu.cs6461.services.cpu.registers.MARImpl;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
