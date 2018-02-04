b99bdfaf81009a712390ab3e6f525f26996ac266
Warren <1988warren@gmail.com>
2018-02-04 04:21:58 -0500

    Implement HALT.

--- src/main/java/com/gwu/cs6461/services/instruction/miscellaneous/HaltImpl.java
+++ src/main/java/com/gwu/cs6461/services/instruction/miscellaneous/HaltImpl.java
@@ -1,5 +1,6 @@
 package com.gwu.cs6461.services.instruction.miscellaneous;
 
+import com.gwu.cs6461.services.cpu.CPUImpl;
 import com.gwu.cs6461.services.instruction.InstructionImpl;
 
 /**
@@ -7,4 +8,21 @@ import com.gwu.cs6461.services.instruction.InstructionImpl;
  */
 public class HaltImpl extends InstructionImpl {
 
+    @Override
+    public Runnable onDecode() {
+        // do nothing
+        Runnable task = () -> {
+
+        };
+        return task;
+    }
+
+    @Override
+    public Runnable onExecute() {
+        // pause CPU
+        Runnable task = () -> {
+            CPUImpl.getInstance().pause();
+        };
+        return task;
+    }
 }
