59016720bd5a6511fbbb30f5e9a2b558665935dd
Warren <1988warren@gmail.com>
2018-02-13 02:20:39 -0500

    comments.

--- src/main/java/com/gwu/cs6461/services/cache/CacheImpl.java
+++ src/main/java/com/gwu/cs6461/services/cache/CacheImpl.java
@@ -1,8 +1,10 @@
 package com.gwu.cs6461.services.cache;
 
 /**
- * Singleton
- * Cache
+ * A fully associative, unified cache.
+ * sits between memory and the rest of the processor.
+ * A vector having a format similar to that described in the lecture notes.
+ * 16 cache lines
  */
 public class CacheImpl implements Cache {
 
