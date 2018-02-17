cb9f7a29bf8c39d9c614f545f01fbd337a55e5ad
qijiuzhi <qijiuzhi@gmail.com>
2018-02-16 19:47:49 -0500

    JGE

--- src/main/java/com/gwu/cs6461/services/instruction/transfer/JGEImpl.java
+++ src/main/java/com/gwu/cs6461/services/instruction/transfer/JGEImpl.java
@@ -1,14 +1,18 @@
 package com.gwu.cs6461.services.instruction.transfer;
 
+import com.gwu.cs6461.services.cpu.registers.IARImpl;
+import com.gwu.cs6461.services.dram.DRAMAddress;
+
 /**
  * JGE Instruction
- * TODO to be implemented
  */
 public class JGEImpl extends TransferImpl {
     @Override
     public Runnable onExecute() {
         Runnable task = () -> {
-
+            if (gpRegister.read().getDecimalValue() >= 0) {
+                IARImpl.getInstance().write(new DRAMAddress().setDecimalValue(effectiveAddress.getDecimalValue()));
+            }
         };
         return task;
     }
