454251c6c85d1f232a4f30b1dd9e34edc00d17f3
qijiuzhi <qijiuzhi@gmail.com>
2018-02-17 15:21:54 -0500

    AMR

--- src/main/java/com/gwu/cs6461/services/instruction/arithmeticlogic/AMRImpl.java
+++ src/main/java/com/gwu/cs6461/services/instruction/arithmeticlogic/AMRImpl.java
@@ -1,17 +1,25 @@
 package com.gwu.cs6461.services.instruction.arithmeticlogic;
 
 
+import com.gwu.cs6461.services.dram.DRAMData;
+import com.gwu.cs6461.services.dram.DRAMDataImpl;
+import com.gwu.cs6461.services.dram.DRAMImpl;
+
 /**
  * AMR Instruction
  * Add Memory To Register, r = 0..3
- * TODO to be implemented
  */
 public class AMRImpl extends ALImpl {
 
     @Override
     public Runnable onExecute() {
         Runnable task = () -> {
-
+            //c(r)
+            DRAMData dataGpR = gpRegister.read();
+            //c(EA)
+            DRAMData dataCEA = DRAMImpl.getInstance().read(effectiveAddress);
+            //r <- c(r) + c(EA)
+            gpRegister.write(new DRAMDataImpl().setDecimalValue(dataGpR.getDecimalValue()+dataCEA.getDecimalValue()));
         };
         return task;
     }
