1f47476a7530841fa2422e5fd9a5d0f59ccb6bc9
houyanbing <hou_yanbing@gwu.edu>
2018-02-18 13:13:32 -0800

    hou-SIR/SMR

--- src/main/java/com/gwu/cs6461/services/instruction/arithmeticlogic/SIRImpl.java
+++ src/main/java/com/gwu/cs6461/services/instruction/arithmeticlogic/SIRImpl.java
@@ -1,18 +1,31 @@
 package com.gwu.cs6461.services.instruction.arithmeticlogic;
 
 
+import com.gwu.cs6461.services.dram.DRAMData;
+import com.gwu.cs6461.services.dram.DRAMDataImpl;
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * SIR Instruction
  * Subtract  Immediate  from Register, r = 0..3
- * TODO to be implemented
  */
 public class SIRImpl extends ALImpl {
-
+    private int immedFieldValue;
     @Override
     public Runnable onExecute() {
         Runnable task = () -> {
+            DRAMData data = gpRegister.read();
+            //r <- c(r) - Immed
+            gpRegister.write(new DRAMDataImpl().setDecimalValue(data.getDecimalValue()  - getImmed()));
 
         };
         return task;
+    }
+
+
+    private int getImmed() {
+        String instructionBinary = toDRAMData().getBinaryValue();
+        immedFieldValue = Integer.parseInt(StringUtils.substring(instructionBinary, 11, 16), 2);
+        return immedFieldValue;
     }
 }
--- src/main/java/com/gwu/cs6461/services/instruction/arithmeticlogic/SMRImpl.java
+++ src/main/java/com/gwu/cs6461/services/instruction/arithmeticlogic/SMRImpl.java
@@ -1,5 +1,9 @@
 package com.gwu.cs6461.services.instruction.arithmeticlogic;
 
+import com.gwu.cs6461.services.dram.DRAMData;
+import com.gwu.cs6461.services.dram.DRAMDataImpl;
+import com.gwu.cs6461.services.dram.DRAMImpl;
+
 /**
  * SMR Instruction
  * Subtract Memory From Register, r = 0..3
@@ -10,7 +14,12 @@ public class SMRImpl extends ALImpl {
     @Override
     public Runnable onExecute() {
         Runnable task = () -> {
-
+            //c(r)
+            DRAMData dataGpR = gpRegister.read();
+            //c(EA)
+            DRAMData dataCEA = DRAMImpl.getInstance().read(effectiveAddress);
+            //r <- c(r) - c(EA)
+            gpRegister.write(new DRAMDataImpl().setDecimalValue(dataGpR.getDecimalValue()-dataCEA.getDecimalValue()));
         };
         return task;
     }
