29545df16d8c0ef83495bd5cc5212f5e7ca65eb3
Warren <1988warren@gmail.com>
2018-02-23 14:00:24 -0500

    decode example.

--- src/main/java/com/gwu/cs6461/services/instruction/io/IOImpl.java
+++ src/main/java/com/gwu/cs6461/services/instruction/io/IOImpl.java
@@ -1,6 +1,9 @@
 package com.gwu.cs6461.services.instruction.io;
 
+import com.gwu.cs6461.services.cpu.registers.GPR0Impl;
 import com.gwu.cs6461.services.cpu.registers.Register;
+import com.gwu.cs6461.services.device.Device;
+import com.gwu.cs6461.services.device.Keyboard;
 import com.gwu.cs6461.services.dram.DRAMData;
 import com.gwu.cs6461.services.instruction.InstructionImpl;
 
@@ -12,11 +15,14 @@ import com.gwu.cs6461.services.instruction.InstructionImpl;
 public class IOImpl extends InstructionImpl {
 
     Register<DRAMData> gpRegister;
+    Device<DRAMData> ioDevice;
 
     @Override
     public Runnable onDecode() {
         Runnable task = () -> {
 
+            gpRegister = GPR0Impl.getInstance();
+            ioDevice = Keyboard.getInstance();
         };
         return task;
     }
