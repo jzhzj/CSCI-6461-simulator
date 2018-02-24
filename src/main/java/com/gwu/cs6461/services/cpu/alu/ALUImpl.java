63a2002b189f8b26cde5e0dd1ed539cc07662198
qijiuzhi <qijiuzhi@gmail.com>
2018-02-24 15:59:47 -0500

    Realized ALU.shift()

--- src/main/java/com/gwu/cs6461/services/cpu/alu/ALUImpl.java
+++ src/main/java/com/gwu/cs6461/services/cpu/alu/ALUImpl.java
@@ -29,7 +29,7 @@ public class ALUImpl implements ALU {
     @Override
     public Binary add(Binary a, int b) {
         // add a and b, then return the result
-        short result = (short) (((short)a.getDecimalValue()) + (short)b);
+        short result = (short) (((short) a.getDecimalValue()) + (short) b);
         int test = a.getDecimalValue() + b;
         // set cc(0) <- true
         if (test > Short.MAX_VALUE) {
@@ -45,25 +45,14 @@ public class ALUImpl implements ALU {
 
     @Override
     public Binary add(Binary a, Binary b) {
-        // add a and b, then return the result
-        short result = (short) (((short)a.getDecimalValue()) + ((short)b.getDecimalValue()));
-        int test = a.getDecimalValue() + b.getDecimalValue();
-        // set cc(0) <- true
-        if (test > Short.MAX_VALUE) {
-            CCRImpl.getInstance().read().setBit(ConditionCode.Type.OVER_FLOW, true);
-        }
-        // set cc(1) <- true
-        if (test < Short.MIN_VALUE) {
-            CCRImpl.getInstance().read().setBit(ConditionCode.Type.UNDER_FLOW, true);
-        }
-        return new DRAMDataImpl().setDecimalValue(result);
+        return add(a,b.getDecimalValue());
     }
 
 
     @Override
     public Binary subtract(Binary a, int b) {
         // subtract b from a, then return the result
-        short result = (short) (((short)a.getDecimalValue()) - (short)b);
+        short result = (short) (((short) a.getDecimalValue()) - (short) b);
         int test = a.getDecimalValue() - b;
         // set cc(0) <- true
         if (test > Short.MAX_VALUE) {
@@ -79,18 +68,7 @@ public class ALUImpl implements ALU {
 
     @Override
     public Binary subtract(Binary a, Binary b) {
-        // subtract b from a, then return the result
-        short result = (short) (((short)a.getDecimalValue()) - ((short)b.getDecimalValue()));
-        int test = a.getDecimalValue() - b.getDecimalValue();
-        // set cc(0) <- true
-        if (test > Short.MAX_VALUE) {
-            CCRImpl.getInstance().read().setBit(ConditionCode.Type.OVER_FLOW, true);
-        }
-        // set cc(1) <- true
-        if (test < Short.MIN_VALUE) {
-            CCRImpl.getInstance().read().setBit(ConditionCode.Type.UNDER_FLOW, true);
-        }
-        return new DRAMDataImpl().setDecimalValue(result);
+        return subtract(a,b.getDecimalValue());
     }
 
 
@@ -98,13 +76,13 @@ public class ALUImpl implements ALU {
     public Binary[] multiply(Binary a, Binary b) {
         DRAMData[] dramData = {new DRAMDataImpl(), new DRAMDataImpl()};
         // a * b
-        int result = (short)a.getDecimalValue() * (short) b.getDecimalValue();
+        int result = (short) a.getDecimalValue() * (short) b.getDecimalValue();
         // set cc(0) <- true
         if (result > (Short.MAX_VALUE * Short.MAX_VALUE)) {
             CCRImpl.getInstance().read().setBit(ConditionCode.Type.OVER_FLOW, true);
         }
-        short r0 = (short) (result>>MachineProps.WORD_BIT_WIDTH);
-        short r1 = (short)result;
+        short r0 = (short) (result >> MachineProps.WORD_BIT_WIDTH);
+        short r1 = (short) result;
         // store the higher order bits
         dramData[0].setDecimalValue(r0);
         // store the lower order bits
@@ -136,7 +114,7 @@ public class ALUImpl implements ALU {
     @Override
     public Binary and(Binary a, Binary b) {
         // Logical And of a and b, then return the result
-        short result = (short)((short)a.getDecimalValue() & (short) b.getDecimalValue());
+        short result = (short) ((short) a.getDecimalValue() & (short) b.getDecimalValue());
         return new DRAMDataImpl().setDecimalValue(result);
     }
 
@@ -144,7 +122,7 @@ public class ALUImpl implements ALU {
     @Override
     public Binary or(Binary a, Binary b) {
         //Logical Or of a and b, then return the result
-        short result = (short)((short)a.getDecimalValue() | (short)b.getDecimalValue());
+        short result = (short) ((short) a.getDecimalValue() | (short) b.getDecimalValue());
         return new DRAMDataImpl().setDecimalValue(result);
     }
 
@@ -152,14 +130,14 @@ public class ALUImpl implements ALU {
     @Override
     public Binary not(Binary a) {
         //Logical Not of a, then return the result
-        short result = (short)(~a.getDecimalValue());
+        short result = (short) (~a.getDecimalValue());
         return new DRAMDataImpl().setDecimalValue(result);
     }
 
 
     @Override
     public void test(Binary a, Binary b) {
-        if ((short)a.getDecimalValue() == (short)b.getDecimalValue()) {
+        if ((short) a.getDecimalValue() == (short) b.getDecimalValue()) {
             // set cc(3) <- true
             CCRImpl.getInstance().read().setBit(ConditionCode.Type.EQ_OR_NOT, true);
         } else {
@@ -172,7 +150,7 @@ public class ALUImpl implements ALU {
     @Override
     public Binary arithmeticShiftR(Binary a, int count) {
         short num = (short) a.getDecimalValue();
-        // Arithmetically Shift register by count
+        // Arithmetically Shift register right by count
         for (int i = 0; i < count; i++) {
             // set cc(1) <- true
             if ((num & 1) == 1) {
@@ -183,17 +161,34 @@ public class ALUImpl implements ALU {
         return new DRAMDataImpl().setDecimalValue(num);
     }
 
-    //TODO to be implemented
+
     @Override
     public Binary logicalShiftL(Binary a, int count) {
-
-        return null;
+        short num = (short) a.getDecimalValue();
+        // Logically Shift register left by count
+        for (int i = 0; i < count; i++) {
+            // set cc(0) <- true
+            if ((num & Short.MIN_VALUE) == Short.MIN_VALUE) {
+                CCRImpl.getInstance().read().setBit(ConditionCode.Type.OVER_FLOW, true);
+            }
+            num = (short) (num << 1);
+        }
+        return new DRAMDataImpl().setDecimalValue(num);
     }
 
-    //TODO to be implemented
+
     @Override
     public Binary logicalShiftR(Binary a, int count) {
-        return null;
+        short num = (short) a.getDecimalValue();
+        // Arithmetically Shift register right by count
+        for (int i = 0; i < count; i++) {
+            // set cc(1) <- true
+            if ((num & 1) == 1) {
+                CCRImpl.getInstance().read().setBit(ConditionCode.Type.UNDER_FLOW, true);
+            }
+            num = (short) (num >>> 1);
+        }
+        return new DRAMDataImpl().setDecimalValue(num);
     }
 
     //TODO to be implemented
