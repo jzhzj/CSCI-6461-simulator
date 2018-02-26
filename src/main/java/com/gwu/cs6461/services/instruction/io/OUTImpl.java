59716d2b967f4f48908efdebe40bb8b114520574
Warren <1988warren@gmail.com>
2018-02-25 23:48:05 -0500

    implement IO instruction.

--- src/main/java/com/gwu/cs6461/services/instruction/io/OUTImpl.java
+++ src/main/java/com/gwu/cs6461/services/instruction/io/OUTImpl.java
@@ -3,13 +3,13 @@ package com.gwu.cs6461.services.instruction.io;
 
 /**
  * OUT Instruction
- * TODO to be implemented
+ * Output Character to Device from Register, r = 0..3
  */
 public class OUTImpl extends IOImpl {
     @Override
     public Runnable onExecute() {
         Runnable task = () -> {
-
+            ioDevice.write(gpRegister.read());
         };
         return task;
     }
