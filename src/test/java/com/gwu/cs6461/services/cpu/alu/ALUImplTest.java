42ff1f175fc8eccaead62ceb1a9b54a40acf825d
Warren <1988warren@gmail.com>
2018-02-26 20:53:07 -0500

    Create ALU unit test.

---
+++ src/test/java/com/gwu/cs6461/services/cpu/alu/ALUImplTest.java
@@ -0,0 +1,76 @@
+package com.gwu.cs6461.services.cpu.alu;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ALUImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void add1() {
+    }
+
+    @Test
+    public void subtract() {
+    }
+
+    @Test
+    public void subtract1() {
+    }
+
+    @Test
+    public void multiply() {
+    }
+
+    @Test
+    public void divide() {
+    }
+
+    @Test
+    public void and() {
+    }
+
+    @Test
+    public void or() {
+    }
+
+    @Test
+    public void not() {
+    }
+
+    @Test
+    public void test1() {
+    }
+
+    @Test
+    public void arithmeticShiftR() {
+    }
+
+    @Test
+    public void logicalShiftL() {
+    }
+
+    @Test
+    public void logicalShiftR() {
+    }
+
+    @Test
+    public void logicalRotateL() {
+    }
+
+    @Test
+    public void logicalRotateR() {
+    }
+}
