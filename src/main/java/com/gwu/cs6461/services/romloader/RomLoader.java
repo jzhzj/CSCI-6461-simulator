ce702b1c95dfc4acc9c09cbf94882671c7e823e9
Warren <1988warren@gmail.com>
2018-03-03 01:43:34 -0500

    change constant reference.

--- src/main/java/com/gwu/cs6461/services/romloader/RomLoader.java
+++ src/main/java/com/gwu/cs6461/services/romloader/RomLoader.java
@@ -1,9 +1,13 @@
 package com.gwu.cs6461.services.romloader;
 
+import com.gwu.cs6461.constants.MachineProps;
+
 /**
  * Defines what Rom Loader does
  */
 public interface RomLoader {
+    String BOOT_FILE_PATH = MachineProps.ROM_BOOT_FILE_PATH;
+
     /**
      * Initialize DRAM and load rom data at from address 8
      */
